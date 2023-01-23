b15dcafb84f0f1287b90fa753fc7690071acfafb
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-23 18:54:29 +0530

    [Shweta] Modified Quotient and Reminder is Calculated

--- BasicCorePrograms_CSharp/Program.cs
+++ BasicCorePrograms_CSharp/Program.cs
@@ -5,8 +5,7 @@ class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("//-----------------Welcome to Basic Core Programs----------------//");
-
-            HarmonicNumber.HarmonicNumberDisplay();
+            QuotientAndReminder.QuotientAndReminderDisplay();
 
         }
     }
---
+++ BasicCorePrograms_CSharp/QuotientAndReminder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace BasicCorePrograms_CSharp
+{
+	public class QuotientAndReminder
+	{
+		public static void QuotientAndReminderDisplay()
+		{
+			Console.WriteLine("Enter the Dividand ");
+			int Dividand = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Divisor ");
+            int Divisor = Convert.ToInt32(Console.ReadLine());
+
+			int Quotient = Dividand / Divisor;
+			int Reminder = Dividand % Divisor;
+			Console.WriteLine("The quotient is {0} and reminder is {1} ", Quotient, Reminder);
+        }
+	}
+}
+
