1f002d3e696870dd8f12c89e12722a0b5016a5e1
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-23 21:24:45 +0530

    [Shweta] Modified Vowels and Consonants are Checked

---
+++ BasicCorePrograms_CSharp/Vowels&Consonants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace BasicCorePrograms_CSharp
+{
+	public class Vowels_Consonants
+	{
+		public static void VowelsandConsonants()
+		{
+			Console.WriteLine("Enter the Alphabet ");
+			char Character = Convert.ToChar(Console.ReadLine());
+
+            if (Character == 'a' || Character == 'e' || Character == 'i' || Character == 'o' || Character == 'u' ||
+             Character == 'A' || Character == 'E' || Character == 'I' || Character == 'O' || Character == 'U'
+             )
+            {
+                Console.WriteLine("Entered Character is Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Entered Character is Consonant");
+            }
+        }
+	}
+}
+
