60dac197025f290e54789f2ec5ac92fa992cbf97
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-23 19:05:44 +0530

    [Shweta] Modified Program Of swap numbers

--- BasicCorePrograms_CSharp/Program.cs
+++ BasicCorePrograms_CSharp/Program.cs
@@ -5,7 +5,7 @@ class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("//-----------------Welcome to Basic Core Programs----------------//");
-            QuotientAndReminder.QuotientAndReminderDisplay();
+            SwapNumber.SwapNumberDisplay();
 
         }
     }
---
+++ BasicCorePrograms_CSharp/SwapNumber.cs
@@ -0,0 +1,23 @@
+﻿
+namespace BasicCorePrograms_CSharp
+{
+	public class SwapNumber
+	{
+	    public static void SwapNumberDisplay()
+		{
+			int temp = 0;
+
+			Console.WriteLine("Enter the first number");
+			int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number");
+            int Second = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Numbers before swaping {0} {1}", first, Second);
+			temp = first;
+			first = Second;
+			Second = temp;
+            Console.WriteLine("Numbers After swaping {0} {1}", first, Second);
+		}
+
+	}
+}
+
