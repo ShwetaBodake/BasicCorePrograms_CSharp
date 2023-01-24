ce55199c782408ea4c813288cf025cf6bf18e5fd
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-24 12:28:31 +0530

    [Shweta] Modified Checked whether given number is prime or not

---
+++ BasicCorePrograms_CSharp/PrimeFactors.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace BasicCorePrograms_CSharp
+{
+	public class PrimeFactors
+	{
+		public static void PrimeFactorsofNumber()
+		{
+			int count = 0;
+			Console.WriteLine("Enter the number");
+			int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("{0} number is prime number", number);
+            }
+            else
+                Console.WriteLine("{0} number is not a prime number", number);
+
+
+
+            //int factor = 1;
+            //for(factor=1;factor<=number ;factor++)
+            //{
+            //	int isPrime = number % factor;
+
+            //             if (isPrime == 0)
+            //	{
+            //		Console.WriteLine("It is a factor of given number {0} ", factor);
+            //                 int temp = factor;
+            //                 for (int i = 1; i <= temp; i++)
+            //                 {
+            //                     if (temp % i == 0)
+            //                     {
+            //                         count++;
+            //                     }
+
+            //                 }
+            //                 if (count == 2)
+            //                 {
+            //                     Console.Write("Prime factors of number {0} ", temp);
+            //                 }
+
+            //             }
+            //}
+
+
+
+
+
+        }
+    }
+}
+
--- BasicCorePrograms_CSharp/Program.cs
+++ BasicCorePrograms_CSharp/Program.cs
@@ -5,7 +5,7 @@ class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("//-----------------Welcome to Basic Core Programs----------------//");
-            LargestNumber.LargestNumberDisplay();
+            PrimeFactors.PrimeFactorsofNumber();
         }
     }
 }
